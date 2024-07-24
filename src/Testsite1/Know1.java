d13225ee0b112b6225a67af28fe75a6891a84d81
amsaranya <amsaranya3@gmail.com>
2024-07-24 22:54:47 +0530

    Sucess2

---
+++ src/Testsite1/Know1.java
@@ -0,0 +1,22 @@
+package Testsite1;
+
+import org.testng.annotations.Test;
+
+public class Know1 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("run1");
+	}
+		
+	@Test
+	public void test2()
+	{
+		System.out.println("run2");
+	}
+		
+
+	
+
+}
